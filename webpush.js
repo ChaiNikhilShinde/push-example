7c64865c1b497fb060edfac6fde69b2f34d09293
Nikhil Shinde <nikhil@chai.finance>
2023-06-14 15:56:13 +0530

    Add new VAPID Key

--- webpush.js
+++ webpush.js
@@ -3,12 +3,12 @@ const webpush = require('web-push');
 // VAPID keys should only be generated only once.
 const vapidKeys = {
   publicKey:
-    'BBZY7Q3KEtZArAAWMLi_qzWHbH4vAoqPpIXnRhmlUaw0PVs1Kt_2fgLhuaVI5i8MWASBKx3d6W6UoH2U3qChw9U',
-  privateKey: 'CZtf_JUxmXkCKbzwaKedPPO9BFC99U2rk-GUYDbYAa8'
+    'BJkwr8YFq0MvT7PxYFrsapQkS82fSHcGDM5t7aGBF4FBlRCM8Pjy1APatWUjrvx-ICk4io54ks7GN78jPqyhZYo',
+  privateKey: 'muV0vo0ls3pC08z8NGdp3YxbleGZLDa3lpt3tCFBaE4'
 };
 
 webpush.setVapidDetails(
-  'mailto:example@yourdomain.org',
+  'mailto: <nikhil@chai.finance>',
   vapidKeys.publicKey,
   vapidKeys.privateKey
 );
